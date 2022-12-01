6dd2a47ed668c6bb4e1cee4be52d411fb224f2e6
Alex Salam <vornat2@gmail.com>
2022-12-01 17:38:13 +0000

    calories array storing the top 3 values and prints total

--- src/calories.rs
+++ src/calories.rs
@@ -5,26 +5,32 @@ use std::path::Path;
 pub fn count()
 {
     if let Ok(lines) = read_lines("./src/data/1/data.txt") {
-        // Consumes the iterator, returns an (Optional) String
 
-        let mut max_calories = 0;
+        let mut max_calories = vec![0, 0, 0];
         let mut cals = 0;
 
         for line in lines {
             if let Ok(item) = line {
                 if str::is_empty(&item) {
-                    if cals > max_calories {
-                        max_calories = cals;
+                    // Ensure the array is always in order
+                    max_calories.sort();
+                    // If the cals is bigger than the smallest value after sorting
+                    if cals > max_calories[0] {
+                        max_calories.remove(0);
+                        max_calories.push(cals);
                     }
                     cals = 0;
-                    continue;
                 } else {
                     cals = cals + item.parse::<i32>().unwrap();
                 }
             }
         }
 
-        println!("{max_calories}");
+        let mut total = 0;
+        for item in max_calories {
+            total = total + item;
+        }
+        println!("Total cals over top 3 elves: {total}");
     }
 }
 
