66509ef23a629255f88b71d465dcd437c54bd464
Alex Salam <vornat2@gmail.com>
2022-12-01 17:19:11 +0000

    calculated max calories correctly

--- src/calories.rs
+++ src/calories.rs
@@ -1,4 +1,35 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 pub fn count()
 {
-    println!("YES!");
+    if let Ok(lines) = read_lines("./src/data/1/data.txt") {
+        // Consumes the iterator, returns an (Optional) String
+
+        let mut max_calories = 0;
+        let mut cals = 0;
+
+        for line in lines {
+            if let Ok(item) = line {
+                if str::is_empty(&item) {
+                    if cals > max_calories {
+                        max_calories = cals;
+                    }
+                    cals = 0;
+                    continue;
+                } else {
+                    cals = cals + item.parse::<i32>().unwrap();
+                }
+            }
+        }
+
+        println!("{max_calories}");
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::process::exit;
 
 // Puzzle modules
 pub mod calories;
@@ -8,13 +7,12 @@ pub mod calories;
 fn main() {
     // Get the descired challange day from the command line
     let args: Vec<String> = env::args().collect();
-    let day = &args[1].parse::<i32>();
+    let day = &args[1].parse::<i8>();
 
     // Validate that this is an acceptable integer
     match day {
         Err(_) => {
             println!("Must be a valid integer");
-            exit(1);
         }
         Ok(day) => {
             println!("Doing puzzle for day: {}", day);
