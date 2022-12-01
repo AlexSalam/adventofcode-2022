f3fe56b40b0416f076feefea05ad10e59711c45b
Alex Salam <vornat2@gmail.com>
2022-12-01 13:33:20 +0000

    Added day selection and module for puzzle 1

---
+++ src/calories.rs
@@ -0,0 +1,4 @@
+pub fn count()
+{
+    println!("YES!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use std::env;
+use std::process::exit;
 
+// Puzzle modules
+pub mod calories;
 
 // Main application
 fn main() {
-    println!("Hello, world!");
+    // Get the descired challange day from the command line
+    let args: Vec<String> = env::args().collect();
+    let day = &args[1].parse::<i32>();
+
+    // Validate that this is an acceptable integer
+    match day {
+        Err(_) => {
+            println!("Must be a valid integer");
+            exit(1);
+        }
+        Ok(day) => {
+            println!("Doing puzzle for day: {}", day);
+            // Run the module method for this puzzle
+            match day {
+                1 => {
+                    calories::count();
+                }
+                _ => {
+                    println!("Values must be between 1 and 25");
+                }
+            }
+        }
+    }
+
 }
